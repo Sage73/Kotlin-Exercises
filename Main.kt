fb6c0551cfbc3762980e22903afc050007316826
George Salib <salibgeorge91372@gmail.com>
2021-07-29 21:09:26 +0200

    greet function - #1

--- Main.kt
+++ Main.kt
@@ -1,3 +1,7 @@
 fun main() {
-    
+    // Make a simple function called greet that returns the most-famous "hello world!".
+
+    fun greet(): String { // creating functions greet that returns a string
+        return "hello world!" // returning "hello world!"
+    }
 }
